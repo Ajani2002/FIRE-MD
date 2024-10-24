25f9939abcb8a5fde279b71d66b5d4c74f49ca9c
Ajani2002 <113563166+Ajani2002@users.noreply.github.com>
2024-10-24 14:34:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
-global.allowed = ['2349013432338', '2349013432338', '2349013432338']
+global.mods = ['2349023382567', '2349023382567']
+global.prems = ['2349023382567', '2349023382567', '2349023382567']
+global.allowed = ['2349023382567', '2349023382567', '2349023382567']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
@@ -72,7 +72,7 @@ global.APIKeys = {
 global.botname = process.env.BOTNAME
 global.premium = 'false'
 global.packname = 'FIRE MD'
-global.author = 'OPAKSFIRE'
+global.author = 'TOXICBOY'
 global.menuvid = 'https://files.catbox.moe/r9oil6.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/\n'
 global.dygp = 'https://whatsapp.com/channel/0029VajYjMJJf05aRQXKx82W'
